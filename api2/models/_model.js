97cb21d9ee9516f1ae798d94b8358e64c690ecca
ThomasTestaud <thomas.testaud@hotmail.fr>
2024-03-06 09:54:31 +0100

    ...

--- api2/models/_model.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelizeInstance = new Sequelize(process.env.DDB_NAME, process.env.DDB_USER, process.env.DDB_PASSWORD, {
-    host: process.env.DDB_HOST, 
-    port: process.env.DDB_PORT,
-    dialect: 'mysql',
-});
-
-module.exports = sequelizeInstance;
