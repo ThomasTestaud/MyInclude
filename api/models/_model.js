304b5a475491c1bfbb1c1df7fb5ccb4cf0c2af83
Thomas Testaud <thomas.testaud@hotmail.fr>
2024-03-09 18:22:27 +0100

    ...

--- api/models/_model.js
+++ api/models/_model.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelizeInstance = new Sequelize(process.env.DDB_NAME, process.env.DDB_USER, process.env.DDB_PASSWORD, {
     host: process.env.DDB_HOST, 
-    port: process.env.DDB_PORT,
+    //port: process.env.DDB_PORT,
     dialect: 'mysql',
 });
 
